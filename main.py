f785acea181d59e5aada7342dc9cbbe0b9a8b879
Tania Semeshchuk <74670974+TaniaSemeshchuk@users.noreply.github.com>
2024-04-26 12:40:39 +0300

    main.py
    Повністю зроблений інтерфейс додатка "Розумні нотатки"

--- main.py
+++ main.py
@@ -4,11 +4,12 @@ from PyQt5.QtWidgets import (QApplication,
                              QTextEdit,
                              QLabel,
                              QListWidget,
-                             QPushButton)
+                             QPushButton, QLineEdit,
+                             QHBoxLayout, QVBoxLayout)
 
 app = QApplication([])
 notes_win = QWidget()
-
+notes_win.resize(900, 600)
 notes_win.setWindowTitle("Розумні нотатки")
 field_text = QTextEdit()
 list_notes_label = QLabel("Список нотаток")
@@ -22,8 +23,46 @@ list_tag = QListWidget()
 btn_tag_add = QPushButton("Додати до нотатки")
 btn_tag_del = QPushButton("Видалити з нотатки")
 btn_tag_search = QPushButton("Шукати нотатку по тегу")
+
+field_tag = QLineEdit('')
+field_tag.setPlaceholderText('Введіть тег')
+
+layout_notes = QHBoxLayout()
+col1 = QVBoxLayout()
+col1.addWidget(field_text)
+
+col2 = QVBoxLayout()
+col2.addWidget(list_notes_label)
+col2.addWidget(list_notes)
+row1 = QHBoxLayout()
+row1.addWidget(btn_create)
+row1.addWidget(btn_del)
+row2 = QHBoxLayout()
+row2.addWidget(btn_save)
+col2.addLayout(row1)
+col2.addLayout(row2)
+
+col2.addWidget(list_tag_label)
+col2.addWidget(list_tag)
+col2.addWidget(field_tag)
+
+row3 = QHBoxLayout()
+row3.addWidget(btn_tag_add)
+row3.addWidget(btn_tag_del)
+row4 = QHBoxLayout()
+row4.addWidget(btn_tag_search)
+col2.addLayout(row3)
+col2.addLayout(row4)
+
+layout_notes.addLayout(col1, stretch = 2)
+layout_notes.addLayout(col2, stretch = 1)
+notes_win.setLayout(layout_notes)
+
 notes_win.show()
 app.exec_()
+
+
+
 
 
 
